2ee8d8cb8ec062490729da1dee4cb06dda265268
Joahan Moise Jordan BAMELA ZÔ'Ô <zoojohan10@gmail.com>
2025-08-24 16:25:59 +0100

    Error_handling for validation problem : fixed

--- middleware/Error-handler.js
+++ middleware/Error-handler.js
@@ -1,35 +1,32 @@
-const {StatusCodes} = require('http-status-codes');
+const {StatusCodes} = require("http-status-codes");
 
-const ErrorHandlerMiddleware = (err, req, res) => {
+const ErrorHandlerMiddleware = (err, req, res, next) => {
     const GenericError = {
-        StatusCode : err.statusCode || StatusCodes.INTERNAL_SERVER_ERRROR,
-        message: err.msg || "Something went wrong try again later",
+        StatusCode : err.status || StatusCodes.INTERNAL_SERVER_ERROR,
+        message: err.message || "Something went wrong try again later",
     };
-
     if (err.name === "ValidationError"){
-        GenericError.msg = Object.values(err.error)
-        .map((items) => items.message)
-        .join(',');
-
+        console.log("Attention")
+        GenericError.message = err.message;
         GenericError.StatusCode = 400;
 
     };
 
     // Handling of Duplicate Error
-    if(err.code || err.code === 11000){
-        GenericError.msg = `Duplicate value entered for ${Object.keys(
+    if(err.code || err.code == 11000){
+        GenericError.message = `Duplicate value entered for ${Object.keys(
             err.keyValue
           )} field, please choose another value`;
-        GenericError.statusCode = 400;
+        GenericError.StatusCode = 400;
     };
 
     // Handling of Cast Error
-    if (err.name === "CastError"){
-        GenericError.msg = `No item found with id : ${err.value}`;
+    if (err.name == "CastError"){
+        GenericError.message = `No item found with id : ${err.value}`;
         GenericError.StatusCode = 404;
     };
 
-    return res.status(GenericError.statusCode).json({ msg: GenericError.msg });
+    return res.status(GenericError.StatusCode).json({ error:"true", message: GenericError.message });
 }
 
 module.exports = ErrorHandlerMiddleware;
